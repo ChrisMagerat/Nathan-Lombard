24a3f685d6469ca3a2fbe00e5b1427b121f88ff0
u20438428 <u20438428@tuks.co.za>
2023-09-07 13:19:15 +0200

    Problem Solver -complete

---
+++ Controllers/ProblemSolverController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nathan.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProblemSolverController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetAmountSeconds")]
+        public async Task<IActionResult> GetAmountSeconds(string word)
+        {
+            try
+            {
+
+                string alphabet = "abcdefhijklmnopqrstuvwxyz";
+
+                
+                int currentPosition = 0;
+                
+                
+                double timeTotal = 0;
+
+                foreach (char c in word)
+                {
+                    
+                    int target = alphabet.IndexOf(c);
+
+                    
+                    int clockwiseTime = Math.Abs(target - currentPosition) * 5;
+                    int counterclockwiseTime = (25 - Math.Abs(target - currentPosition)) * 5;
+
+                  
+                    int moveTime = Math.Min(clockwiseTime, counterclockwiseTime);
+
+  
+                    if (moveTime == 0)
+                    {
+                        timeTotal += 2.5;
+                    }
+                    else
+                    {
+                        timeTotal += moveTime;
+                    }
+
+                    currentPosition = target;
+                }
+
+
+                return Ok(timeTotal);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Internal Server Error. Please contact support.");
+            }
+        }
+    }
+}
